8b275b46551817499e3eb4ac4727b8a0a162d7b9
Chris Pineda <zhris@zhr.is>
2024-02-01 10:46:38 -0600

    Initial commit.

---
+++ NanoSettings Watch App/NanoSettingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NanoSettingsApp.swift
+//  NanoSettings Watch App
+//
+//  Settings
+//
+
+import SwiftUI
+
+@main
+struct NanoSettings_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
