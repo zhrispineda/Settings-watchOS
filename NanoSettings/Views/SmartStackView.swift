ac178dbd036e7182daa77e37017ff2f9d6eab3d9
zhrispineda <zhris@zhr.is>
2025-08-23 15:04:16 -0500

    Update Localizable, revert SmartStackView string changes

--- NanoSettings/Views/SmartStackView.swift
+++ NanoSettings/Views/SmartStackView.swift
@@ -8,16 +8,12 @@
 import SwiftUI
 
 struct SmartStackView: View {
-    let liveActivities = "/System/Library/PreferenceBundles/CSLNanoLiveActivitiesSettings.bundle"
-    let smartStack = "/System/Library/PrivateFrameworks/SmartStackSettingsUI.framework"
-    let autoLaunchTable = "NanoAutoLaunchSettings"
-    
     var body: some View {
         List {
-            NavigationLink("LIVE_ACTIVITIES_TITLE".localized(path: liveActivities, table: autoLaunchTable), destination: LiveActivitiesView())
-            NavigationLink("SMART_STACK_SUGGESTION_SETTINGS_NAVIGATION_TITLE".localized(path: smartStack), destination: WidgetSuggestionsView())
+            NavigationLink("SMART_STACK_LIVE_ACTIVITIES_TITLE", destination: LiveActivitiesView())
+            NavigationLink("SMART_STACK_TITLE", destination: WidgetSuggestionsView())
         }
-        .navigationTitle("LIVE_ACTIVITIES_AUTOLAUNCH_SETTING_SMARTSTACK".localized(path: liveActivities, table: autoLaunchTable))
+        .navigationTitle("SMART_STACK_TITLE")
     }
 }
 
