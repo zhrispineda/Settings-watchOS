68d951c4a8c38f8693783cee9ab3e96faa8a7a8d
zhrispineda <zhris@zhr.is>
2025-08-23 15:10:45 -0500

    Update GeneralView strings

--- NanoSettings/Views/GeneralView.swift
+++ NanoSettings/Views/GeneralView.swift
@@ -10,18 +10,18 @@ import SwiftUI
 struct GeneralView: View {
     var body: some View {
         List {
-            NavigationLink("About", destination: AboutView())
-            NavigationLink("Software Update", destination: SoftwareUpdateView())
-            NavigationLink("Orientation", destination: OrientationView())
-            NavigationLink("Auto-Launch", destination: AutoLaunchView())
-            NavigationLink("Background App Refresh", destination: BackgroundAppRefreshView())
-            NavigationLink("Return to Clock", destination: ReturnClockView())
-            NavigationLink("Nightstand Mode", destination: NightstandModeView())
-            NavigationLink("Handoff", destination: HandoffView())
-            NavigationLink("Website Data", destination: WebsiteDataView())
-            NavigationLink("Profiles", destination: ProfilesView())
+            NavigationLink("ABOUT_TITLE", destination: AboutView())
+            NavigationLink("SOFTWARE_UPDATE_TITLE", destination: SoftwareUpdateView())
+            NavigationLink("ORIENTATION_TITLE", destination: OrientationView())
+            NavigationLink("AUTO_LAUNCH_APP_TITLE", destination: AutoLaunchView())
+            NavigationLink("BACKGROUND_APP_UPDATING_LABEL", destination: BackgroundAppRefreshView())
+            NavigationLink("RETURN_TO_CLOCK_CELL_TITLE", destination: ReturnClockView())
+            NavigationLink("NIGHTSTAND_MODE_TITLE", destination: NightstandModeView())
+            NavigationLink("CONTINUITY_TITLE", destination: HandoffView())
+            NavigationLink("SAFARI_TITLE", destination: WebsiteDataView())
+            NavigationLink("PROFILES_MDM_TITLE", destination: ProfilesView())
         }
-        .navigationTitle("General")
+        .navigationTitle("STATUS_BAR_TITLE_GENERAL")
     }
 }
 
