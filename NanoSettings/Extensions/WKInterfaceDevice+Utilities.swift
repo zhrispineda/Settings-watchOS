299f1dfdee25f064541ec212b6eba98691a89d9f
zhrispineda <zhris@zhr.is>
2025-10-28 20:35:09 -0400

    Add comment to IsSimulated

--- NanoSettings/Extensions/WKInterfaceDevice+Utilities.swift
+++ NanoSettings/Extensions/WKInterfaceDevice+Utilities.swift
@@ -6,6 +6,7 @@
 import SwiftUI
 
 extension WKInterfaceDevice {
+    /// Returns whether the host is on a simulated/preview instance or on a physical device.
     static let IsSimulated: Bool = {
         if let answer = MGHelper.read(key: "ulMliLomP737aAOJ/w/evA") { // IsSimulator key
             return Bool(answer)!
