3ff0bf4484e769f4feb5f84278e65cf039a7740c
zhrispineda <zhris@zhr.is>
2026-01-04 09:59:42 -0500

    Update WorkoutView

--- NanoSettings/Views/WorkoutView.swift
+++ NanoSettings/Views/WorkoutView.swift
@@ -8,22 +8,37 @@
 import SwiftUI
 
 struct WorkoutView: View {
-    // Variables
     @AppStorage("LowPowerModeToggle") private var lowPowerModeEnabled = false
+    private let fitnessPath = "/System/Library/PrivateFrameworks/FitnessUI.framework"
+    private let path = "/System/Library/PreferenceBundles/WorkoutSettings.bundle"
+    private let meadowTable = "Localizable-meadow"
+    private let table = "Settings"
+    private var footerText: String {
+        "WORKOUT_LOW_POWER_MODE_FOOTER".localized(path: fitnessPath, table: meadowTable)
+    }
+    private var footerLinkText: String {
+        "WORKOUT_LOW_POWER_MODE_FOOTER_LEARN_MORE_LINK".localized(path: fitnessPath, table: meadowTable)
+    }
     
     var body: some View {
         List {
             Section {
-                Toggle("Low Power Mode", isOn: $lowPowerModeEnabled)
+                Toggle("WORKOUT_MODE_LOW_POWER_LABEL".localized(path: fitnessPath), isOn: $lowPowerModeEnabled)
             } footer: {
-                Text("Turn on Low Power Mode during a workout. Learn more about [Low Power Mode...](#)")
+                Text(.init(footerText.replacing(
+                    footerLinkText,
+                    with: "[\(footerLinkText)](pref://lowpowermode)")
+                ))
             }
             
             Section {
-                NavigationLink("Units of Measure", destination: UnitsMeasureView())
+                NavigationLink("UNITS_OF_MEASURE_LABEL".localized(path: path, table: table), destination: UnitsMeasureView())
             }
         }
-        .navigationTitle("Workout Data")
+        .navigationTitle("SETTINGS_TITLE_UNINSTALLED".localized(path: path, table: table))
+        .task {
+            lowPowerModeEnabled = ProcessInfo().isLowPowerModeEnabled
+        }
     }
 }
 
