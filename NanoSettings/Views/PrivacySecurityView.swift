05a8ff16dd46aa0fd1e3d3a4bf2fbfb619852086
zhrispineda <zhris@zhr.is>
2026-01-10 13:46:40 -0500

    Update PrivacySecurityView layout

--- NanoSettings/Views/PrivacySecurityView.swift
+++ NanoSettings/Views/PrivacySecurityView.swift
@@ -2,37 +2,96 @@
 //  PrivacySecurityView.swift
 //  NanoSettings
 //
-//  Settings > Privacy & Security
-//
 
 import SwiftUI
 
+/// View for Settings > Privacy & Security
 struct PrivacySecurityView: View {
     var body: some View {
         List {
             Section {
-                SettingsLink("Location Services", icon: "location.circle.fill", primaryColor: .white, secondaryColor: .blue, content: { LocationServicesView() })
-                NavigationLink(destination: AppPermissionsDetailView(title: "Contacts"), label: {
-                    IconLabel(title: "Contacts", icon: "contacts")
-                })
-                SettingsLink("Calendar", icon: "calendar.circle.fill", primaryColor: .red, secondaryColor: .white, content: { AppPermissionsDetailView(title: "Calendars") })
-                NavigationLink(destination: { AppPermissionsDetailView(title: "Reminders") }, label: {
+                SettingsLink(
+                    "Location Services",
+                    icon: "location.circle.fill",
+                    primaryColor: .white,
+                    secondaryColor: .blue,
+                    content: { LocationServicesView() }
+                )
+                NavigationLink(
+                    destination: AppPermissionsDetailView(title: "Contacts"),
+                    label: {
+                        IconLabel(title: "Contacts", icon: "contacts")
+                    }
+                )
+                SettingsLink(
+                    "Calendar",
+                    icon: "calendar.circle.fill",
+                    primaryColor: .red,
+                    secondaryColor: .white,
+                    content: {
+                        AppPermissionsDetailView(title: "Calendars")
+                    }
+                )
+                NavigationLink(destination: AppPermissionsDetailView(title: "Reminders")) {
                     IconLabel(title: "Reminders", icon: "Reminders")
-                })
-                SettingsLink("Bluetooth", icon: "bluetooth", primaryColor: .white, secondaryColor: .blue, content: { AppPermissionsDetailView(title: "Bluetooth") })
-                SettingsLink("Microphone", icon: "mic.circle.fill", primaryColor: .white, secondaryColor: .red, content: { AppPermissionsDetailView(title: "Microphone") })
-                NavigationLink(destination: HealthView(), label: {
+                }
+                SettingsLink(
+                    "Bluetooth",
+                    icon: "bluetooth",
+                    primaryColor: .white,
+                    secondaryColor: .blue,
+                    content: {
+                        AppPermissionsDetailView(title: "Bluetooth")
+                    }
+                )
+                SettingsLink(
+                    "Microphone",
+                    icon: "mic.circle.fill",
+                    primaryColor: .white,
+                    secondaryColor: .red,
+                    content: {
+                        AppPermissionsDetailView(title: "Microphone")
+                    }
+                )
+                NavigationLink(destination: HealthView()) {
                     IconLabel(title: "Health", icon: "applehealth")
-                })
-                NavigationLink(destination: AppPermissionsDetailView(title: "HomeKit"), label: {
+                }
+                NavigationLink(destination: AppPermissionsDetailView(title: "HomeKit")) {
                     IconLabel(title: "HomeKit", icon: "applehome")
-                })
-                SettingsLink("Motion & Fitness", icon: "figure.run.motion.circle.fill", primaryColor: .white, secondaryColor: .green, content: { AppPermissionsDetailView(title: "Motion & Fitness") })
-                NavigationLink(destination: AppPermissionsDetailView(title: "Analytics & Improvements"), label: {
-                    IconLabel(title: "Analytics & Improvements", icon: "com.apple.graphic-icon.analytics-and-improvements")
-                })
-                SettingsLink("Fall Detection", icon: "figure.fall.circle.fill", primaryColor: .white, secondaryColor: .blue, content: { AppPermissionsDetailView(title: "Fall Detection Data Sharing") })
-                SettingsLink("Focus", icon: "moon.circle.fill", primaryColor: .white, secondaryColor: .indigo, content: { AppPermissionsDetailView(title: "Focus") })
+                }
+                SettingsLink(
+                    "Motion & Fitness",
+                    icon: "figure.run.motion.circle.fill",
+                    primaryColor: .white,
+                    secondaryColor: .green,
+                    content: {
+                        AppPermissionsDetailView(title: "Motion & Fitness")
+                    }
+                )
+                NavigationLink(destination: AppPermissionsDetailView(title: "Analytics & Improvements")) {
+                    IconLabel(
+                        title: "Analytics & Improvements",
+                        icon: "com.apple.graphic-icon.analytics-and-improvements"
+                    )
+                }
+                SettingsLink(
+                    "Fall Detection",
+                    icon: "figure.fall.circle.fill",
+                    primaryColor: .white,
+                    secondaryColor: .blue,
+                    content: {
+                        AppPermissionsDetailView(title: "Fall Detection Data Sharing")
+                    }
+                )
+                SettingsLink(
+                    "Focus",
+                    icon: "moon.circle.fill",
+                    primaryColor: .white,
+                    secondaryColor: .indigo,
+                    content: {
+                        AppPermissionsDetailView(title: "Focus")
+                    }
+                )
             } footer: {
                 Text("Some privacy settings are shared between Apple Watch and iPhone. You can manage these settings in the Privacy section of iPhone settings.")
             }
